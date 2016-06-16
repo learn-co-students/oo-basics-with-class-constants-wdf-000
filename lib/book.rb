970755cf7fe6a9c43c579d916687171f5f43ed04
uu <giladbarkan@live.com>
2016-06-16 14:09:14 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,19 @@
+require 'set'
+
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  # used set to avoid duplicating genre entries, although lesson example uses an array
+  GENRES = Set.new
 
   def initialize(title)
     @title = title
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
   end
 
   def turn_page
