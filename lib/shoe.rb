b5fed93ca449bbfab7dcfe1d53d2602ff7d03c65
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-07-18 21:29:09 -0400

    class_constants

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  
+  BRANDS = []
   def initialize(brand)
     @brand = brand
+    # since we are creating instances like this Shoe.new(brand)
+    self.brand = brand 
   end
 
   def cobble
@@ -11,4 +15,12 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
+  end
+
 end
+
+
