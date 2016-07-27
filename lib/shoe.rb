dce115828c3176c7386f031eb3ab28b3364b36c5
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-07-27 14:15:31 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,11 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
     #BRANDS << brand unless BRANDS.include?(brand)
     # since we are creating instances like this Shoe.new(brand)
-    self.brand = brand 
+    # self.brand = brand 
   end
 
   def cobble
@@ -18,10 +20,44 @@ class Shoe
 
   def brand=(brand)
     @brand = brand
-    BRANDS << brand
-    BRANDS.uniq!
   end
 
 end
+
+
+# class Dogs 
+
+#   @@dogs = []
+
+#   def initialize(name)
+#     @name = name
+#     @@dogs << self
+#   end
+
+#   def bark 
+#     "WOOF"
+#   end
+
+#   def greet
+#     "my name is #{name}"
+#   end
+
+#   def self.how_many_dogs
+#     puts "There are #{count} dogs in your kennel"
+#   end
+
+#   def self.count 
+#     @@dogs.size 
+#   end
+
+#   def self.list_dogs
+#     @@dogs.each do |dog|
+#       puts dog.name
+#     end
+#   end
+
+# end
+
+
 
 
